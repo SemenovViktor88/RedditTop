7d371010391716b23de1d5f5941fbbc195739043
Semenov Viktor <viktorsemenov88@gmail.com>
2023-03-30 13:08:50 +0300

    test...

--- app/src/main/java/com/semenov/reddit/data/presentation/MainActivity.kt
+++ app/src/main/java/com/semenov/reddit/data/presentation/MainActivity.kt
@@ -4,6 +4,7 @@ import android.os.Bundle
 import android.util.Log
 
 import androidx.appcompat.app.AppCompatActivity
+import androidx.lifecycle.lifecycleScope
 import com.semenov.reddit.data.model.Common
 import com.semenov.reddit.databinding.ActivityMainBinding
 import kotlinx.coroutines.GlobalScope
@@ -19,19 +20,15 @@ class MainActivity : AppCompatActivity() {
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
 
-
-
         val topApi = Common.retrofitService
 
-
-
-        // launching a new coroutine
-        GlobalScope.launch {
-            val result = topApi.getTopList()
-
-            if (result != null)
-            // Checking the results
+        binding.btn.setOnClickListener {
+            lifecycleScope.launch {
+                val result = topApi.getTopList()
                 Log.d("Mylog: ", result.toString())
+                binding.textView.text = result.toString()
+            }
         }
+
     }
 }
