f7de51d7848df681c37889a8579039939a26b675
Semenov Viktor <viktorsemenov88@gmail.com>
2023-03-31 14:42:21 +0300

    update ApiRaddit

--- app/src/main/java/com/semenov/reddit/data/model/ApiReddit.kt
+++ app/src/main/java/com/semenov/reddit/data/model/ApiReddit.kt
@@ -1,6 +1,10 @@
 package com.semenov.reddit.data.model
 
 data class ApiReddit(
+    val subreddit: String? = null,
+    val title: String? = null,
+    val thumbnail: String? = null,
     val author: String? = null,
     val url: String? = null,
+    val num_comments: Int? = null,
 )
