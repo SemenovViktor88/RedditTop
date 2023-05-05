5705ebe4507e53a3aaf9647028c29e7e35be04c9
Semenov Viktor <viktorsemenov88@gmail.com>
2023-05-05 10:43:37 +0300

    .

--- app/src/main/java/com/semenov/reddit/presentation/main/ViewPagerAdapter.kt
+++ app/src/main/java/com/semenov/reddit/presentation/main/ViewPagerAdapter.kt
@@ -4,14 +4,9 @@ package com.semenov.reddit.presentation
 import androidx.fragment.app.Fragment
 import androidx.fragment.app.FragmentActivity
 import androidx.viewpager2.adapter.FragmentStateAdapter
-import com.semenov.reddit.presentation.MainFragment
-import com.semenov.reddit.presentation.NewsFragment
-import com.semenov.reddit.presentation.SaveFragment
+
 ========
 package com.semenov.reddit.presentation.main
-import androidx.fragment.app.Fragment
-import androidx.fragment.app.FragmentActivity
-import androidx.viewpager2.adapter.FragmentStateAdapter
 >>>>>>>> 8233821 (create click listener for open info news):app/src/main/java/com/semenov/reddit/presentation/main/ViewPagerAdapter.kt
 
 class ViewPagerAdapter(fragmentActivity : FragmentActivity, private val list: List<Fragment>): FragmentStateAdapter(fragmentActivity) {
